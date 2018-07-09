913fb5bb9aa98322131e5c044256b9bfdc9c0757
wuxiaopeng <1099272020@qq.com>
2018-07-09 15:25:55 +0800

    提交配置变动

--- cloud-config-client/src/main/java/com/example/config/ConfigClient.java
+++ cloud-config-client/src/main/java/com/example/config/ConfigClient.java
@@ -2,11 +2,14 @@ package com.example.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-@SpringBootApplication
+
 //// 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
-@RefreshScope
+//@RefreshScope
+@EnableDiscoveryClient
+@SpringBootApplication
 public class ConfigClient {
 
 	public static void main(String[] args) {
