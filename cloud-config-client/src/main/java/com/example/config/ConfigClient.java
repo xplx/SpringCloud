bdb2943d26f01f52df12cfce5364f2aafd3e4372
wuxiaopeng <1099272020@qq.com>
2018-07-09 13:46:19 +0800

    配置问价修改

--- cloud-config-client/src/main/java/com/example/config/ConfigClient.java
+++ cloud-config-client/src/main/java/com/example/config/ConfigClient.java
@@ -2,8 +2,11 @@ package com.example.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+//// 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
+@RefreshScope
 public class ConfigClient {
 
 	public static void main(String[] args) {
